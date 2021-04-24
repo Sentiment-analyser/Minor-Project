dd25fba776878b4d56c5ec622f6284ee41d67777
Aditya Mukhopadhyay <66019008+Andr-o-meda@users.noreply.github.com>
2021-04-24 13:01:40 +0530

    Update data_collector.py

--- data_collector.py
+++ data_collector.py
@@ -1,11 +1,16 @@
-import tweepy,json,pandas,csv
+import tweepy,json,pandas,csv,nltk,string,re  # for pattern matching, used for removing URLs, Usernames and Punctuations
+import pandas as pd
 import urllib.request
 from bs4 import BeautifulSoup
+from nltk.corpus import stopwords # for removing stopwords
+from nltk.tokenize import RegexpTokenizer # for tokenizing
+from nltk.stem import WordNetLemmatizer # for lemmatizing
+from nltk.stem.porter import PorterStemmer # for stemming
 
 
 
 def search_tokens():
-    url='https://www.thesaurus.com/browse/sad?s=t'  
+    url='https://www.thesaurus.com/browse/happy?s=t'  
     '''
     #IF you are willing to search for customised tags uncomment the code snippet, then enter a word and all related tags 
     #shall be automatically searched
@@ -24,8 +29,9 @@ def search_tokens():
     # reference: <a font-weight="inherit" href="/browse/dismal" data-linkid="nn1ov4" class="css-1m14xsh eh475bn1">dismal<!-- --> </a>
     bs = BeautifulSoup(read_content, 'html.parser')
     h1 = bs.find_all('a', class_="css-1m14xsh eh475bn1")    #data filter phase
-    t_d=[]
+    t_d=['sad']
     for i in h1:
+        #z='#'+i.text
         t_d.append(i.text)
     return t_d
 
@@ -35,7 +41,7 @@ def search_tokens():
 def raw_polished():     #raw data from Twitter is converted to a polished version where noise and other metadatas are removed
     ok=False
     l,ml,cnt=[],[],0
-    with open('tweet.txt','r') as file:
+    with open('Plus.txt','r') as file:
         for line in file:
             for word in line.split():  
                 if(word=='\"text\":' or word=='\"description\":'):
@@ -53,20 +59,40 @@ def raw_polished():     #raw data from Twitter is converted to a polished versio
                     w=w+word+' '
                     #print(w)      #debugging (check whether the variable is actually storing something)
     #print(ml)                     #debugging (check the final list for desired values) 
-    with open('converted.csv','w') as f:
+    with open('Plusconv.csv','w') as f:
         write = csv.writer(f) 
         write.writerow(['POST','DESCRIPTION'])  #the different tags to choose from are present in the update log 2021.02.06
         write.writerows(ml)
     
+
+
+def remove_un(text):       #removing noise from the data
     
-    
+    free= re.sub(r'http\S+', '', text)
+    free= re.sub(r'u0+','',free)
+    free= re.sub(r'\\ud\S+', '', free)
+    free= re.sub(r'\xa0\S+', '', free)
+    free= re.sub(r'pic.\S+', '', free)
+    free= re.sub('@[\w]+','',free)         #removing usernames
+    free= "".join([char for char in free if char not in string.punctuation]) #removing punctuations
+    return free 
+
+def stop_lemm_stem(text):   #removing stopwords , lemmatizing and stemwords
+    hs= (open('hindiswords.txt','r').readlines())
+    free=[word for word in text if word not in stopwords.words('english')+stopwords.words('french')+hs]
+    lemmatizer=WordNetLemmatizer()
+    free=[lemmatizer.lemmatize(word) for word in free]
+    stemmer=PorterStemmer()
+    free=" ".join([stemmer.stem(word) for word in free])
+    return free
+
 def PHASE_1(READ=True):
     if(READ):                                           #since this process can be time consuming, data collection from Twitter everytime can
                                                         #be skipped by passing False as parameter 
-        access_token="*"
-        access_token_secret="*"
-        consumer_key="*"
-        consumer_secret="*"
+        access_token="1356115757050937347-hIaRlwZpgGafrGhaMHjruo72PUGf4v"
+        access_token_secret="GstHAPyelXZlZtFsXu0ieAEQlX4BPUfZpnqjoYRCWrPrq"
+        consumer_key="LUE8ITDfE08n3ERjWpXyx0ZWc"
+        consumer_secret="ydOXjuc6RhYKogpJ42fKNodT8qV5NTXzoZFny5LAKFOo4VubqR"
         auth= tweepy.OAuthHandler(consumer_key,consumer_secret)
         auth.set_access_token(access_token,access_token_secret)
         tweet_list=[]
@@ -74,7 +100,7 @@ def PHASE_1(READ=True):
             def __init__(self,api=None):
                 super(MyStreamListener,self).__init__()
                 self.num_tweets=0
-                self.file=open("tweet.txt","w")
+                self.file=open("Plus.txt","w")
             def on_status(self,status):
                 tweet=status._json
                 self.file.write(json.dumps(tweet)+ '\n')
@@ -88,7 +114,7 @@ def PHASE_1(READ=True):
         l = MyStreamListener()
         stream =tweepy.Stream(auth,l)
         stream.filter(track=search_tokens())
-        tweets_data_path='tweet.txt'
+        tweets_data_path='Plus.txt'
         tweets_data=[]
         tweets_file=open(tweets_data_path,"r")
         #read in tweets and store on list
@@ -101,9 +127,27 @@ def PHASE_1(READ=True):
         read_file.to_csv ('Untitled Folder/converted.csv', index=None)'''
         raw_polished()                #creates a CSV file after removing all the unnecessary data
     
-def main():
-    PHASE_1(False)      #data collection phase
+def PHASE_2(PROCESS=True):
+    if(nltk.download('stopwords') and nltk.download('wordnet')):
+        print("prerequistes checked")
+        data=pd.read_csv("Depression_Tweet.csv")
+        data.shape 
+        pd.set_option('display.max_colwidth',None)
+        
+        data['POST']=[str(tweet) for tweet in data['POST']]
+        
+        data['POST']=data['POST'].apply(lambda x:remove_un(x)) #list(data['TWEET'])
+        
+        tokenizer=RegexpTokenizer(r'\w+')
+        data['POST']=data['POST'].apply(lambda x:tokenizer.tokenize(x.lower()))
+        
+        data['POST']=data['POST'].apply(lambda x:stop_lemm_stem(x))
+        print(data) #to check the final contents of preprocessed data
     
+def main():
+    PHASE_1()      #data collection phase
+    PHASE_2()
+    print('All complete')
 if __name__ == "__main__":
     main()
     
